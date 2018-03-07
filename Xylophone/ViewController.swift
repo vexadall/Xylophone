3a2e47fa790f3e57f66d8547500a462e1932b681
Alex <vexadall@gmail.com>
2018-03-07 15:53:49 +0200

    Add xylophone functionality based on button's tag.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        let soundURL = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav")
+        
+            do {
+            
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL! )
+            
+            }catch{
+                
+            print(error)
+        }
+        
+        audioPlayer.play()
     }
-
+    
 
 }
 
